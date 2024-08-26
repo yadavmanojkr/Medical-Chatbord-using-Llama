d5a77eeae2e9f343f0ff4071100a32d2b159816a
Manoj Kr Yadav <manoj.yadav415@gmail.com>
2024-08-26 19:10:35 +0530

    data added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.1',
+    author= 'Manoj Kr Yadav',
+    author_email= 'manoj.yadav415@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
